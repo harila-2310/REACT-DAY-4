06035a44a16b7668a95997c32fb40189a0e5d251
harila-2310 <hariprasathmg@gmail.com>
2023-03-31 13:47:04 +0530

    msg

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GreetingForm from './Components/Greetings';
+
+function App() {
+  return (
+    <div className="App">
+      <GreetingForm/>
+    </div>
+  );
+}
+
+export default App;
